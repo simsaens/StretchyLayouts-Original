f0b5cf70f2452be3f397be2c401fa661c08e9da0
Simeon Saëns <simeon@twolivesleft.com>
2017-10-31 15:29:49 +1030

    Changed view background colour

--- StretchyLayouts/StretchyViewController.swift
+++ StretchyLayouts/StretchyViewController.swift
@@ -22,6 +22,8 @@ class StretchyViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .gray
+        
         imageView.image = UIImage(named: "Header")
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
